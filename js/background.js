846c33908d1bba54e3d050a480fd25f4351de290
Catherine Vo <catherinevo@Catherines-MacBook-Pro.local>
2018-04-19 13:59:50 -0500

    date form fields added with defaults range set to current date

--- js/background.js
+++ js/background.js
@@ -1,7 +1,18 @@
 'use strict';
 
+
+
 chrome.runtime.onInstalled.addListener(function() {
   
     console.log('Background tasks will run here');
-  
+
+    //if 10K login authentication is true, show  index.html
+
+    //else show 10K login screen
+
+
 });
+
+
+
+
--- js/logHours.js
+++ js/logHours.js
@@ -5,9 +5,26 @@
 'use strict';
 
 let logHours = document.getElementById('logHours');
+let fromDate = document.getElementById('fromDate');
+let toDate = document.getElementById('toDate');
 
+$(function(){
+  fromDate.value = getCurrentDate();
+  toDate.value = getCurrentDate();
+});
 
 logHours.onclick = function(element) {
-  console.log("YEAHHH we're logging some hours now!");
-  
+  console.log("YEAHHH we're logging some hours now!");  
 };
+
+
+function getCurrentDate(){
+  var fullDate = new Date();
+  //console.log(fullDate);
+  var twoDigitMonth = fullDate.getMonth()+1+"";if(twoDigitMonth.length==1)  twoDigitMonth="0" +twoDigitMonth;
+  var twoDigitDate = fullDate.getDate()+"";if(twoDigitDate.length==1) twoDigitDate="0" +twoDigitDate;
+  var currentDate = fullDate.getFullYear() + "-" +  twoDigitMonth + "-" +  twoDigitDate;
+  //console.log(currentDate);
+  return currentDate;
+}
+
